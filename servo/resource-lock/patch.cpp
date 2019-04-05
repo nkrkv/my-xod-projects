b5fc324b1a56e2209b810ef40e0d645c4917a4e6
Victor Nakoryakov <nail.xx@gmail.com>
2019-04-05 18:59:01 +0300

    refactor(servo): make `resourse-lock` input generic

--- servo/resource-lock/patch.cpp
+++ servo/resource-lock/patch.cpp
@@ -5,32 +5,32 @@ struct State {
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
-    auto xservo = getValue<input_DEV>(ctx);
+    auto res = getValue<input_RES>(ctx);
 
     if (isSettingUp()) {
-        // Short-circuit DEV and DEV'
-        emitValue<output_DEVU0027>(ctx, xservo);
+        // Short-circuit RES and RES'
+        emitValue<output_RESU0027>(ctx, res);
     }
 
     bool justEntered = false;
     auto nodeId = getNodeId(ctx);
 
-    if (isInputDirty<input_ENTR>(ctx) && xservo->lockedFor == 0) {
-        xservo->lockedFor = nodeId;
+    if (isInputDirty<input_ENTR>(ctx) && res->lockedFor == 0) {
+        res->lockedFor = nodeId;
         justEntered = true;
     }
 
     if (isInputDirty<input_EXIT>(ctx)
-        && (xservo->lockedFor == 0 || xservo->lockedFor == nodeId)) {
+        && (res->lockedFor == 0 || res->lockedFor == nodeId)) {
 
         justEntered = false;
-        xservo->lockedFor = 0;
+        res->lockedFor = 0;
         emitValue<output_EXITU0027>(ctx, 1);
     }
 
     if (justEntered)
         emitValue<output_ENTRU0027>(ctx, 1);
 
-    if (isInputDirty<input_IN>(ctx) && xservo->lockedFor == nodeId)
+    if (isInputDirty<input_IN>(ctx) && res->lockedFor == nodeId)
         emitValue<output_OUT>(ctx, 1);
 }
